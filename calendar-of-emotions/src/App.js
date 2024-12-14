749c71a86e508ea534d57c8a202cdd70c14e4a13
Tatsiana <badpanda@tut.by>
2024-12-14 23:18:37 +0300

    UserProfile component (filters)  was implemented

--- calendar-of-emotions/src/App.js
+++ calendar-of-emotions/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import EventsComponent from './components/EventsComponent';
 import Cards from './components/Cards/Cards.js';
 import Header from './components/Header/Header.js'
+import UserProfile from './components/UserProfile.js';
 function App() {
   return (
     <div className="app-container">
       <Header />
       <Cards />
       <EventsComponent />
+      <UserProfile/>
     </div>
   );
 }
